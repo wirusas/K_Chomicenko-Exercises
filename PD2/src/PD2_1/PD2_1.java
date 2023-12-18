30103b221ef71211a40817c77ac75b443588a645
Kazimieras Chomicenko <kazischomicenko@gmail.com>
2023-12-18 22:10:14 +0200

    tasks 2_1 - 2_4 done

---
+++ PD2/src/PD2_1/PD2_1.java
@@ -0,0 +1,9 @@
+package PD2_1;
+
+public class PD2_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun");
+    }
+}
