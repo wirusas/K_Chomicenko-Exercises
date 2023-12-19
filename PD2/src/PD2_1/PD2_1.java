cf94e95740cc548418421d4fad573e0fc1740636
Kazimieras Chomicenko <kazischomicenko@gmail.com>
2023-12-19 19:05:01 +0200

    PD2 update

--- PD2/src/PD2_1/PD2_1.java
+++
@@ -1,9 +0,0 @@
-package PD2_1;
-
-public class PD2_1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java");
-        System.out.println("Learning Java Now");
-        System.out.println("Programming is fun");
-    }
-}
