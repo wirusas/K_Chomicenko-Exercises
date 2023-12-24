1553f141341e6ef92acffd0eeed788e0bdce30ac
kazis <kazis>
2023-12-24 23:10:11 +0200

    PD 4_7 done

---
+++ PD4/src/lt/pd4_7/New4_7.java
@@ -0,0 +1,25 @@
+package lt.pd4_7;
+
+import java.util.Scanner;
+
+public class New4_7 {
+    public static void main(String[] args) {
+        String[] weekDays = {"Monday", "Tuesdat", "Wednesday", "Thursday", "Friday"};
+//int lesonsPerDay = 0;
+        int lessonsTotal = 0;
+        int minutesPerLesson = 45;
+        int minutesTotal = 0;
+
+        for (int i = 0; i < weekDays.length; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter how many lessons on " + weekDays[i] + " ");
+
+            int lessonsPerDay = Integer.parseInt(scanner.nextLine());
+            lessonsTotal += lessonsPerDay;
+            minutesTotal = lessonsTotal * minutesPerLesson;
+        }
+        System.out.println("You have " + lessonsTotal + " lessons per week");
+        System.out.println("You have " + minutesTotal + " minutes of lessons per week");
+
+    }
+}
